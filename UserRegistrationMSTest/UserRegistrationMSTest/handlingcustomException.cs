b8ddaab30378ff9bcbf1a66951151d5533808f7e
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-24 20:10:56 +0530

    Day 21 Assignment Problems

---
+++ UserRegistrationMSTest/UserRegistrationMSTest/handlingcustomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistrationMSTest
+{
+    public class handlingcustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            WRONG_INPUT
+        }
+        private readonly ExceptionType type;
+        public handlingcustomException(ExceptionType Type, string message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
--- UserRegistrationMSTest/UserRegistrationMSTestCases/UnitTest1.cs
+++ UserRegistrationMSTest/UserRegistrationMSTestCases/UnitTest1.cs
@@ -15,12 +15,26 @@ namespace UserRegistrationMSTestCases
             Assert.IsTrue(check);
         }
         [TestMethod]
+        [DataRow("suchindra")]
+        public void CheckIfFirstNameIsValidorNot(string firstname)
+        {
+            ExceptionUseCases validate = new ExceptionUseCases();
+            Assert.ThrowsException<handlingcustomException>(() => validate.FirstName(firstname));
+        }
+        [TestMethod]
         [DataRow("Chitnis")]
         public void CheckLastNameIsValidOrNot(string lastname)
         {
             UseCases validate = new UseCases();
             bool check = validate.LastName(lastname);
             Assert.IsTrue(check);
+        }
+        [TestMethod]
+        [DataRow("chitnis")]
+        public void CheckIfLastNameIsValidOrNot(string lastname)
+        {
+            ExceptionUseCases validate = new ExceptionUseCases();
+            Assert.ThrowsException<handlingcustomException>(() => validate.LastName(lastname));
         }
         [TestMethod]
         [DataRow("abc.xyz@bl.co.in")]
@@ -31,6 +45,13 @@ namespace UserRegistrationMSTestCases
             Assert.IsTrue(check);
         }
         [TestMethod]
+        [DataRow("abcxyz@bl.co.in")]
+        public void CheckIfEmailIdIsValidOrNot(string Emailid)
+        {
+            ExceptionUseCases validate = new ExceptionUseCases();
+            Assert.ThrowsException<handlingcustomException>(() => validate.EmailId(Emailid));
+        }
+        [TestMethod]
         [DataRow("91 9045678934")]
         public void CheckMobileNumberIsValidOrNot(string Phonenumber)
         {
@@ -39,12 +60,26 @@ namespace UserRegistrationMSTestCases
             Assert.IsTrue(check);
         }
         [TestMethod]
-        [DataRow("suchindrA@32")]
+        [DataRow("9045678934")]
+        public void CheckIfMobileNumberIsValidOrNot(string Phonenumber)
+        {
+            ExceptionUseCases validate = new ExceptionUseCases();
+            Assert.ThrowsException<handlingcustomException>(() => validate.PhoneNumber(Phonenumber));
+        }
+        [TestMethod]
+        [DataRow("suchindrA@456")]
         public void CheckPasswordIsValidOrNot(string Password)
         {
             UseCases validate = new UseCases();
             bool check = validate.Password1(Password);
             Assert.IsTrue(check);
+        }
+        [TestMethod]
+        [DataRow("suchindra456")]
+        public void CheckIfPasswordIsValidOrNot(string Password)
+        {
+            ExceptionUseCases validate = new ExceptionUseCases();
+            Assert.ThrowsException<handlingcustomException>(() => validate.Password1(Password));
         }
     }
 }
